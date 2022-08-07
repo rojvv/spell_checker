f6ef6a662aa7cd1fd74b9d701b2890947e281bd5
Roj <ez@roj.im>
2022-08-08 02:13:14 +0300

    Update README and redirect requests other than POST

--- README.md
+++ README.md
@@ -12,3 +12,23 @@
 ```shell
 deno run --allow-ffi --unstable main.ts
 ```
+
+## The Server
+
+There's an experimental HTTP server that can be tried:
+
+```shell
+deno --allow-env --allow-ffi --allow-net --allow-read --allow-write --unstable server.ts
+```
+
+You can then send requests like this:
+
+```ts
+await fetch("http://server", {
+  method: "POST",
+  headers: { "content-type": "application/json" },
+  body: JSON.stringify("somee bad sentense to chek"),
+});
+```
+
+There's also a running instance of the HTTP server at <https://dsp.roj.im>.
--- server.ts
+++ server.ts
@@ -1,8 +1,9 @@
+import { serve } from "https://deno.land/std@0.151.0/http/mod.ts";
 import * as log from "https://deno.land/std@0.151.0/log/mod.ts";
 import { Mutex } from "https://cdn.skypack.dev/async-mutex?dts";
-import { serve } from "https://deno.land/std@0.151.0/http/mod.ts";
 import { SpellChecker } from "./spell_checker.ts";
 
+// A sort of mutex is used to keep everything safe because Hunspell is not thread-safe.
 const mutex = new Mutex();
 const checker = new SpellChecker("./en_US.aff", "./en_US.dic");
 
@@ -19,45 +20,52 @@ await log.setup({
 
 serve(async (req, connInfo) => {
   if (
-    req.method == "POST" &&
-    req.headers.get("content-type") == "application/json"
+    req.method == "POST"
   ) {
-    const content = await req.json();
     if (
-      typeof content === "string" && content.length > 0 &&
-      // deno-lint-ignore no-control-regex
-      content.length <= 4096 && /^[\x00-\x7F]*$/.test(content)
+      req.headers.get("content-type") == "application/json"
     ) {
-      const parts = content.split(/(\W)/);
-      const result = new Array<[string, { note?: string; correct: boolean }]>();
-      const waitStart = Date.now();
-      const release = await mutex.acquire();
-      const waitEnd = Date.now();
-      const start = Date.now();
-      for (const part of parts) {
-        const test = /^[A-z]*$/.test(part);
-        const note = test ? undefined : "ignored";
-        const correct = test ? true : checker.check(part);
-        result.push([part, { note, correct }]);
+      const content = await req.json();
+      if (
+        typeof content === "string" && content.length > 0 &&
+        // deno-lint-ignore no-control-regex
+        content.length <= 4096 && /^[\x00-\x7F]*$/.test(content)
+      ) {
+        const parts = content.split(/(\W)/);
+        const result = new Array<
+          [string, { note?: string; correct: boolean }]
+        >();
+        const waitStart = Date.now();
+        const release = await mutex.acquire();
+        const waitEnd = Date.now();
+        const start = Date.now();
+        for (const part of parts) {
+          const test = /^[A-z]*$/.test(part);
+          const note = test ? undefined : "ignored";
+          const correct = test ? true : checker.check(part);
+          result.push([part, { note, correct }]);
+        }
+        const end = Date.now();
+        release();
+        const checkTime = String(end - start);
+        const waitTime = String(waitEnd - waitStart);
+        log.info(
+          `checked ${parts.length} parts for ${
+            req.headers.get("x-forwarded-for") ??
+              (connInfo.remoteAddr as Deno.NetAddr).hostname
+          } in ${checkTime} with ${waitTime} of wait`,
+        );
+        return new Response(JSON.stringify(result), {
+          headers: {
+            "content-type": "application/json",
+            "x-check-time": checkTime,
+            "x-wait-time": waitTime,
+          },
+        });
       }
-      const end = Date.now();
-      release();
-      const checkTime = String(end - start);
-      const waitTime = String(waitEnd - waitStart);
-      log.info(
-        `checked ${parts.length} parts for ${
-          req.headers.get("x-forwarded-for") ??
-            (connInfo.remoteAddr as Deno.NetAddr).hostname
-        } in ${checkTime} with ${waitTime} of wait`,
-      );
-      return new Response(JSON.stringify(result), {
-        headers: {
-          "content-type": "application/json",
-          "x-check-time": checkTime,
-          "x-wait-time": waitTime,
-        },
-      });
     }
+    return new Response(JSON.stringify("400 Bad Request"), { status: 400 });
   }
-  return new Response(JSON.stringify("400 Bad Request"), { status: 400 });
+  // Good idea, @dcdunkan.
+  return Response.redirect("https://github.com/roj1512/spell_checker");
 }, { port: Number(Deno.env.get("PORT")) || 3000 });
